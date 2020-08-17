ee5181acdda8e35e02ebd34661143e53323495e1
Clément Bonnet <clement.bonnet16@gmail.com>
2020-08-17 16:47:28 +0200

    changed import statement

--- primelib/setup.py
+++ primelib/setup.py
@@ -1,18 +1,18 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setuptools.setup(
+setup(
     name="primelib-clement-bonnet", # Replace with your own username
-    version="0.0.3",
+    version="0.0.1",
     author="Clément Bonnet",
     author_email="clement.bonnet16@gmail.com",
     description="A small package to work with prime numbers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clement-bonnet/medium-upload-pypi",
-    packages=setuptools.find_packages(),
+    url="https://github.com/clement-bonnet/medium-first-package",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
